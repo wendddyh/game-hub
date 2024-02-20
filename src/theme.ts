aff2ef972d88d01cf3bf64e6e5c6ccaea3e382ea
wendyhenry <wendddyhenry@gmail.com>
2024-02-20 16:43:11 +1100

    customize the color theme

--- src/theme.ts
+++ src/theme.ts
@@ -5,6 +5,24 @@ import { extendTheme } from "@chakra-ui/react";
 const config ={ initialColorMode: 'dark'}
 
 //extend the theme
-const theme = extendTheme({config});
+const theme = extendTheme({
+  config,
+  colors: {
+   gray: {
+    50: '#f9f9f9',
+    100: '#ededed',
+    200: '#d3d3d3',
+    300: '#a0a0a0',
+    400: '#898989',
+    500: '#6c6c6c',
+    600: '#202020',
+    700: '121212',
+    800: '#121212',
+    900: '#111'
+   }
+  }
+
+
+});
 
 export default theme
