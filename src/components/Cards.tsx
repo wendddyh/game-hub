ce037321620e8e3fbffe0a6093bdd0361fe6168f
wendyhenry <wendddyhenry@gmail.com>
2024-02-21 11:24:00 +1100

    refactor the grid

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -14,9 +14,9 @@ export const Cards = ({gameQuery}: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={5}
@@ -35,6 +35,6 @@ export const Cards = ({gameQuery}: Props) => {
         ))}
         ;
       </SimpleGrid>
-    </>
+
   );
 };
